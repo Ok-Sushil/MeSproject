6dcd03f64aea422474a9c154dba14ff28f7aed05
Ok-Sushil <sushil26saini@gmail.com>
2023-05-15 18:45:58 +0530

    My Synopsis first commit

---
+++ synopsis/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+import Home from './Components/home/home';
+import Register from './Components/register/register';
+import Login from './Components/login/login';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+  <Home/>
+  {/* <Register/>
+  <Login/> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ synopsis/src/Components/home/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css"
+
+const Register=()=>{
+       return(
+        <div className="homepage">
+              <h1> Hello Home Page
+              </h1>
+              <div className="button"> Logout</div>
+        </div>
+       )
+}
+
+export default Register
